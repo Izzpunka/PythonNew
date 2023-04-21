d9c572f516d85e0735f83b6733c8171a7bf2d043
Elena <harahonova@mail.ru>
2023-04-21 21:30:16 +0300

    Task18

--- DZ3/Zadanie18.py
+++ DZ3/Zadanie18.py
@@ -10,3 +10,19 @@
 #     -> 5
 
 
+from random import randint
+n = int(input("Количество элементов массива: "))
+a = [randint(1,11) for i in range(n)]
+print(a)
+x = int(input("Какое число ищем? "))
+
+min_diff = abs(x - a[0])
+closest = a[0]
+
+for i in a:
+    diff = abs(x - i)
+    if diff < min_diff:
+        min_diff = diff
+        closest = i
+
+print ("Ближайший элемент к числу", x, "в массиве", a, "это", closest)  
